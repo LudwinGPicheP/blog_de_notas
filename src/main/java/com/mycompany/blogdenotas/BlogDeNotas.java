d10d346fe46bf4621e332e0a0829fd1db8834ba1
LudGianPiche <lpichep@proton.me>
2024-01-11 18:36:08 -0600

    subiendo proyecto

---
+++ src/main/java/com/mycompany/blogdenotas/BlogDeNotas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.blogdenotas;
+
+/**
+ *
+ * @author mond
+ */
+public class BlogDeNotas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
